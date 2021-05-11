7d820979d28aaed80c68ab97b9d99e2c3f5a8a0b
Angie  Flower <82205086+AngieFlower@users.noreply.github.com>
2021-05-11 09:22:13 +0200

    commit initial

---
+++ src/main/java/org/factof5/grupo2sharedtodolist/Grupo2sharedtodolistApplication.java
@@ -0,0 +1,13 @@
+package org.factof5.grupo2sharedtodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo2sharedtodolistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupo2sharedtodolistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/factof5/grupo2sharedtodolist/Grupo2sharedtodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package org.factof5.grupo2sharedtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo2sharedtodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
