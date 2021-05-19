7843c08d6c698a3db8fc6812a204d55f0e573db2
Taiza18 <taizabrito@hotmail.com>
2021-05-19 10:40:57 +0200

    ultimos cambios Taiza

---
+++ storetaiza/src/main/java/com/example/storetaiza/StoretaizaApplication.java
@@ -0,0 +1,13 @@
+package com.example.storetaiza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoretaizaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoretaizaApplication.class, args);
+    }
+
+}
